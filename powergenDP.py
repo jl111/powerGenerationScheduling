c8e90a670a7d6a0cfc6980666b564790f8b6854f
Julio Ledesma <jl111@users.noreply.github.com>
2018-02-16 17:00:09 -0600

    Add files via upload

--- powergenDP.py
+++ powergenDP.py
@@ -24,13 +24,22 @@ H.reverse()
 
 
 P = defaultdict(int)
+# P[0] = 100
+# P[1] = -200
+# P[2] = 300
+# P[3] = -100
+# P[4] = -100
+# P[5] = 200
 P[0] = 200
 P[1] = -200
 P[2] = -300
 P[3] = 700
 P[4] = -400
 P[5] = 100
-
+# P[6] = 200
+# P[7] = -100
+# P[8] = 200
+# P[9] = 300
 # for x in range(0, HOURS):
 # 	P[x] = random.randint(-40,100)
 
@@ -76,7 +85,7 @@ for h in H:
 print(M)
 print(" ")
 
-s_prime = 3#max(M[0], key=M[0].get)
+s_prime = max(M[0], key=M[0].get)
 print("S': " + str(s_prime))
 max_profit = M[0][s_prime]
 print("Max Profit: " + str(max_profit))
@@ -92,30 +101,45 @@ for h in H:
 		print("hours: " + str(h))
 		print(s_prime)
 		s_prime += 1
+		print(s_prime)
+		print(M[h][s_prime])
 	elif -MIN_OFF < s_prime and s_prime < 0:
 		print("-MIN_OFF < s_prime and s_prime < 0")
 		print("hours: " + str(h))
 		print(s_prime)
 		s_prime -= 1
+		print(s_prime)
+		print(M[h][s_prime])
 	elif s_prime == MIN_ON:
 		print("s_prime == MIN_ON")
 		print("hours: " + str(h))
 		print(s_prime)
+		print("bye")
+		print(h)
+		print(M[h+1][s_prime]) 
+		print(M[h][-1])
+		print("hi")
 		#if M[h-1][s_prime] - P[h-1] == M[h][s_prime]
 		if M[h-1][s_prime] == M[h][-1]:
 			s_prime = -1
+			print(s_prime)
+			print(M[h][s_prime])
 	elif s_prime == -MIN_OFF:
 		print("s_prime == -MIN_OFF")
 		print("hours: " + str(h))
 		print(s_prime)
-		if M[h-1][s_prime] + START == M[h][1]:
+		if M[h-1][s_prime] + START - P[h-1] == M[h][1]:
 			s_prime = 1
+			print(s_prime)
+			print(M[h][s_prime])
 	else:
 		print("hours: " + str(h))
 		print(s_prime)
 		s_prime = s_prime
+		print(M[h][s_prime])
 
 	V.append(is_on(s_prime))
+print(max_profit)
 print(V)
 
 
